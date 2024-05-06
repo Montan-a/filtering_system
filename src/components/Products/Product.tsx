6a356d64763fbeb2c554e2aa65fe5e7bca7d7101
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-05-05 20:45:18 -0500

    initial commit

---
+++ src/components/Products/Product.tsx
@@ -0,0 +1,28 @@
+import { Product } from "@/db";
+import Image from "next/image";
+
+const Products = ({ product }: { product: Product }) => {
+  return (
+    <div className="group relative">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+        <img
+          src={product.imageId}
+          alt="product image"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <p className="mt-1 text-sm text-gray-500">
+              Size {product.size.toUpperCase()}, {product.color}
+            </p>
+          </h3>
+        </div>
+        <p className="text-sm font-medium text-gray-900">{product.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
